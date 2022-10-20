99e0a493ef46277179826943149a43a19d2a3109
Leire Herranz <leire.herranz@avantgardeit.es>
2022-10-20 18:39:16 +0200

    table

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("homework #27");
+        System.out.println("multiplication table for 3");
+
+        for (int i=0; i<10; i++) {
+            System.out.println("3 * " + i + " = " + (i*3));
+        }
+    }
+}
